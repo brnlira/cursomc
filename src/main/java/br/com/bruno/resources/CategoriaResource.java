6f7f80d659c97e9dade85b9b8f8c1befe40e9a59
Bruno Lira <brunomarqueslira@outlook.com>
2021-11-17 13:48:45 -0300

    Testando a primeira classe de domínio

--- src/main/java/br/com/bruno/resources/CategoriaResource.java
+++ src/main/java/br/com/bruno/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.bruno.resources;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
-	}
+import br.com.bruno.domain.Categoria;
 
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+
+		return categorias;
+	}
 }
