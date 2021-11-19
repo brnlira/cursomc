e3b76a332eab9ff8d3dc8c0f546d459b53831850
Bruno Lira <brunomarqueslira@outlook.com>
2021-11-19 11:44:21 -0300

    Estado e Cidade

--- src/main/java/br/com/bruno/CursomcApplication.java
+++ src/main/java/br/com/bruno/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.bruno.domain.Categoria;
+import br.com.bruno.domain.Cidade;
+import br.com.bruno.domain.Estado;
 import br.com.bruno.domain.Produto;
 import br.com.bruno.repositories.CategoriaRepository;
+import br.com.bruno.repositories.CidadeRepository;
+import br.com.bruno.repositories.EstadoRepository;
 import br.com.bruno.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,10 +53,21 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		/*Instanciando Estados*/
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		/*Instanciando Cidades*/
+		Cidade c1 = new Cidade(null, "Uberlância", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
 		/*Salvando dados no banco de dados*/
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
-	}
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
+	}
 }
+
